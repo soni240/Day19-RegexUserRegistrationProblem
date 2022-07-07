fa0bfe0337a455d96b1f698fef97b99621710b48
soni240 <s.fegade21@gmail.com>
2022-07-07 14:55:44 +0530

    [Gunavanti]Add:UC9_Validate_SampleEmail_Test

--- User_Registration_Problem/Program.cs
+++ User_Registration_Problem/Program.cs
@@ -1,18 +1,125 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using User_Registration_Problem
+using User_Registration_Problem;
 
 Console.WriteLine("----- Regex User Validation -----\n");
 
 RegexOps pattern = new RegexOps();
 
-Console.Write("Enter Password of the User :- ");
-string User_Pass = Console.ReadLine();
-if (pattern.ValidateUserName(User_Pass))
+//Console.Write("Enter Password of the User :- ");
+//string Sample_Mail_ID = Console.ReadLine();
+
+if (pattern.ValidateUserName("abc@yahoo.com"))
 {
-    Console.WriteLine("\n>> User Password format is valid....");
+    Console.WriteLine("Test case 1:");
+    Console.WriteLine("\n>> Sample Email_ID format is valid....");
 }
 else
 {
-    Console.WriteLine("\n>> User Password format is Invalid....");
+    Console.WriteLine("Test case 1:");
+    Console.WriteLine("\n>> Sample Email_ID format is Invalid....");
+}
+
+//---------------------------------------------------------------------------------
+
+if (pattern.ValidateUserName("abc-100@yahoo.com"))
+{
+    Console.WriteLine("\nTest case 2:");
+    Console.WriteLine("\n>> Sample Email_ID format is valid....");
+}
+else
+{
+    Console.WriteLine("\nTest case 2:");
+    Console.WriteLine("\n>> Sample Email_ID format is Invalid....");
+}
+
+//---------------------------------------------------------------------------------
+
+if (pattern.ValidateUserName("abc.100@yahoo.com"))
+{
+    Console.WriteLine("\nTest case 3:");
+    Console.WriteLine("\n>> Sample Email_ID format is valid....");
+}
+else
+{
+    Console.WriteLine("\nTest case 3:");
+    Console.WriteLine("\n>> Sample Email_ID format is Invalid....");
+}
+
+//---------------------------------------------------------------------------------
+
+if (pattern.ValidateUserName("abc111@abc.com"))
+{
+    Console.WriteLine("\nTest case 4:");
+    Console.WriteLine("\n>> Sample Email_ID format is valid....");
+}
+else
+{
+    Console.WriteLine("\nTest case 4:");
+    Console.WriteLine("\n>> Sample Email_ID format is Invalid....");
+}
+
+//---------------------------------------------------------------------------------
+
+if (pattern.ValidateUserName("abc-100@abc.net"))
+{
+    Console.WriteLine("\nTest case 5:");
+    Console.WriteLine("\n>> Sample Email_ID format is valid....");
+}
+else
+{
+    Console.WriteLine("\nTest case 5:");
+    Console.WriteLine("\n>> Sample Email_ID format is Invalid....");
+}
+
+//---------------------------------------------------------------------------------
+
+if (pattern.ValidateUserName("abc.100@abc.com.au"))
+{
+    Console.WriteLine("\nTest case 6:");
+    Console.WriteLine("\n>> Sample Email_ID format is valid....");
+}
+else
+{
+    Console.WriteLine("\nTest case 6:");
+    Console.WriteLine("\n>> Sample Email_ID format is Invalid....");
+}
+
+//---------------------------------------------------------------------------------
+
+if (pattern.ValidateUserName("abc@1.com"))
+{
+    Console.WriteLine("\nTest case 7:");
+    Console.WriteLine("\n>> Sample Email_ID format is valid....");
+}
+else
+{
+    Console.WriteLine("\nTest case 7:");
+    Console.WriteLine("\n>> Sample Email_ID format is Invalid....");
+}
+
+//---------------------------------------------------------------------------------
+
+if (pattern.ValidateUserName("abc@gmail.com.com"))
+{
+    Console.WriteLine("\nTest case 8:");
+    Console.WriteLine("\n>> Sample Email_ID format is valid....");
+}
+else
+{
+    Console.WriteLine("\nTest case 8:");
+    Console.WriteLine("\n>> Sample Email_ID format is Invalid....");
+}
+
+//---------------------------------------------------------------------------------
+
+if (pattern.ValidateUserName("abc+100@gmail.com"))
+{
+    Console.WriteLine("\nTest case 9:");
+    Console.WriteLine("\n>> Sample Email_ID format is valid....");
+}
+else
+{
+    Console.WriteLine("\nTest case 9:");
+    Console.WriteLine("\n>> Sample Email_ID format is Invalid....");
 }
 
--- User_Registration_Problem/RegexOps.cs
+++ User_Registration_Problem/RegexOps.cs
@@ -11,19 +11,19 @@ namespace User_Registration_Problem
     {
         //public static string RegexRule = "(^[A-Z][A-Za-z]{3,20})+( [A-Z]{1}[A-Za-z]{3,20})$"; //First_Last Name Validation
 
-        // public static string RegexRule = "^([A-Za-z0-9]{3,20})([.][A-Za-z0-9]{1,10})*([@][A-Za-z]{2,5})+[.][A-Za-z]{2,3}([.][A-Za-z]{2,3})?$";//Validate Email_ID
+        public static string RegexRule = "^([A-Za-z0-9]{3,20})([.+-_*][A-Za-z0-9]{1,10})*([@][A-Za-z0-9]{1,5})+[.][A-Za-z]{2,3}([.][A-Za-z]{2,3})?$";//Validate Email_ID
 
         // public static string RegexRule = "^[1-9]{2} [6-9]{1}[0-9]{9}$";  // validate Mobile Number
 
-        public static string RegexRule = "(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$_])[a-zA-Z0-9@#$_]{8}";
+        //public static string RegexRule = "(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$_])[a-zA-Z0-9@#$_]{8}"; 
         //Validate password which has atleast one Uppercase char and atleast one number with one special char too
-        public bool ValidateUserName(string User_Pass)
+
+        public bool ValidateUserName(string Sample_Mail_ID)
         {
             {
-                return Regex.IsMatch(User_Pass, RegexRule);
+                return Regex.IsMatch(Sample_Mail_ID, RegexRule);
 
             }
-
         }
     }
 }
