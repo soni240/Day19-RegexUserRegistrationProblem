889f736f1ef5283e38289274f2cbbecaa2567372
soni240 <s.fegade21@gmail.com>
2022-07-07 14:11:56 +0530

    [Gunavanti]Add:UC1_Valid_UserFirstName

---
+++ User_Registration_Problem/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using User_Registration_Problem;
+
+Console.WriteLine("----- Regex User Validation -----\n");
+
+RegexOps pattern = new RegexOps();
+
+Console.Write("Enter the First Name of the User :- ");
+string username = Console.ReadLine();
+if (pattern.ValidateUserName(username))
+{
+    Console.WriteLine("\n>> UserName format is Valid....");
+}
+else
+{
+    Console.WriteLine("\n>> UserName format is Invalid....");
+}
+
---
+++ User_Registration_Problem/RegexOps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User_Registration_Problem
+{
+    public class RegexOps
+    {
+        public static string RegexRule = "^[A-Z][A-Za-z]{3}";
+
+        public bool ValidateUserName(string username)
+        {
+            {
+                return Regex.IsMatch(username, RegexRule);
+
+            }
+        }
+    }
+}
